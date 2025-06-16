3a6983a3d4913d3011aadcfc6db0386ff52eca70
Baran Taşcı <barantasci44@gmail.com>
2025-06-16 13:23:53 +0300

    Ödev#5

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,4 @@
-const getTotalBalanceGender = (users, gender) => {
+const getTotalBalanceByGender = (users, gender) => {
     return users.reduce((total, user) => {
         return user.gender === gender ? total + user.balance : total;
     }, 0);
